dcbd6d56e173f5920d17bfdc4f386afe1d7c770e
Nonso Chinwuba <nonsohchinwuba@gmail.com>
2026-06-23 13:24:11 +0100

    cleaned up the main and app files also creating the home, auth and checkout pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import GlobalState from "./context/testContext";
+
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import ShoppingCartProvider from "./Shopping Cart/context/productContext.jsx";
-import AuthState from "./firbase/context/authContext.jsx";
+
 import { ToastContainer } from "react-toastify";
-import { TaskManagerProvider } from "./Task Manager/context/TasKContext.jsx";
 
 const queryClient = new QueryClient();
 
@@ -17,18 +15,8 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       {/* Query Client Provider for Tanstack Query */}
       <QueryClientProvider client={queryClient}>
-        {/* Context from our Shopping Cart Project */}
-        <ShoppingCartProvider>
-          {/* Global contextAPI wrapper for global state mgt */}
-          <GlobalState>
-            <AuthState>
-              <TaskManagerProvider>
-                <App />
-                <ToastContainer />
-              </TaskManagerProvider>
-            </AuthState>
-          </GlobalState>
-        </ShoppingCartProvider>
+        <App />
+        <ToastContainer />
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
